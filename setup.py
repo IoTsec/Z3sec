9ba67c4a0b6a44979ddd8a6ff293078f183ab946
IoTsec <philipp.morgner@fau.de>
2017-05-19 12:15:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup (
     name="z3sec",
     version="0.1",
     description="Penetration testing framework to test the touchlink commissioning features of ZigBee-certified products",
-    scripts=['tools/z3sec_touchlink','tools/z3sec_control','tools/z3sec_key_extract','tools/z3sec_show','tools/z3sec_install_code],
+    scripts=['tools/z3sec_touchlink','tools/z3sec_control','tools/z3sec_key_extract','tools/z3sec_show','tools/z3sec_install_code'],
     packages=['z3sec'],
     license="MIT",
     url="https://github.com/IoTsec/Z3sec",
